b95fbce3e78f1874daf489e28d5137b000c965a9
sahil-raj <sahilrajaaru@gmail.com>
2024-08-05 10:38:32 +0530

    btb

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Check from "./pages/UI/Check";
 import EditProfile from "./pages/UI/EditProfile";
-import './index.css';
+import "./index.css";
 
 function App() {
-
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signin" element={<Signin />} />
-                {/* Redirect to Check if authenticated, otherwise to Signin */}
-                <Route path="/check" element={<Check />} />
-                <Route path="/editprofile" element={<EditProfile />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        {/* Redirect to Check if authenticated, otherwise to Signin */}
+        <Route path="/check" element={<Check />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
